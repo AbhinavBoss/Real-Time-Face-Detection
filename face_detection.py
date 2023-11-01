a28d8fcc0e04ca12d696150502457a854cc8151d
Abhinav Varshney <115035256+AbhinavBoss@users.noreply.github.com>
2023-11-02 01:19:22 +0530

    Update face_detection.py

--- face_detection.py
+++ face_detection.py
@@ -1,45 +1,25 @@
 # Face detection on webcam 
 import cv2
+
+#Add Haarcascade pre-trained xml file for face detetction
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml') 
+
+#Capture the video through webcam (0 for default camera)
 cap = cv2.VideoCapture(0) 
-cap.set(3, 640)
-cap.set(4, 480)
 while 1:  
 	_ , img = cap.read() 
+
+	#Conversion of video to gray scale for face detection
 	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
 	faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+	
+	#Draw the rectangle
 	for (x,y,w,h) in faces: 
 		cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,0),2) 
 	cv2.imshow('img',img) 
+	
+	#press q to exit
 	if cv2.waitKey(30) & 0xff==ord('q'): 
 		break
 cap.release() 
 cv2.destroyAllWindows() 
-
-# face detection on image
-# import cv2
-# face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-# img = cv2.imread('3.png')
-# gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-# faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-# for (x, y, w, h) in faces:
-#     cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
-# cv2.imshow('img', img)
-# cv2.waitKey()
-# cv2.destroyAllWindows()
-
-# import cv2
-# def detect_faces(image_path):
-#     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-#     img = cv2.imread(image_path)
-#     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-#     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-#     for (x, y, w, h) in faces:
-#         cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-#     return img
-# image_paths = ['3.png', '4.png'] 
-# for image_path in image_paths:
-#     result_image = detect_faces(image_path)
-#     cv2.imshow('Detected Faces', result_image)
-#     cv2.waitKey(0)
-# cv2.destroyAllWindows()
